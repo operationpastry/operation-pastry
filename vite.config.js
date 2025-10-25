b4be2edcf236a6744c443bba6d938a3e97e3c755
Joshua Lee <me@joshualee.co.nz>
2025-10-25 21:41:58 +1300

    making deployable

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/operation-pastry/', // Set base for GitHub Pages
 })
