82fe28cfbe15e6585a1d816ede6949ac907be2a2
Joshua Lee <me@joshualee.co.nz>
2025-10-26 17:06:21 +1300

    changing names of things

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set base for GitHub Pages
+  base: '/operation-pastry', // Set base for GitHub Pages
 })
